ff510bac335aa91617f6c73892f036d884c4acb0
kydos <kydos@users.noreply.github.com>
2026-03-10 11:15:25 +0000

    deploy: 03ed943d289c21020aee5e3b6f65e78635a698d6

--- _next/static/chunks/app/[locale]/about/page-e28a38adf0e539db.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[853],{4788:function(e,n,s){Promise.resolve().then(s.bind(s,9441)),Promise.resolve().then(s.t.bind(s,5878,23))}},function(e){e.O(0,[441,878,971,117,744],function(){return e(e.s=4788)}),_N_E=e.O()}]);
